98ae597c70067df77ad0101ca6e3bf9de2f631ae
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-05-10 19:38:54 +0300

    complete task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const refs = {
+start: document.querySelector('button[data-start]'),
+stop: document.querySelector('button[data-stop]'),
+}
+
+let intervalID = null
+let isActive = false
+refs.start.disabled = false
+refs.stop.disabled = true
+
+refs.start.addEventListener('click', onStart)
+refs.stop.addEventListener('click', onStop)
+
+function onStart(e) {
+    if (isActive) {
+        return
+    }
+intervalID = setInterval((switchColors), 1000)
+isActive = true
+    refs.start.disabled = true
+    refs.stop.disabled = false
+}
+
+function onStop(e) {
+    clearInterval(intervalID)
+    isActive = false
+    refs.start.disabled = false
+    refs.stop.disabled = true
+}
+ 
+function switchColors() {
+let randomColorRGB = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`
+    document.body.style.background = `${randomColorRGB}`
+    console.log(randomColorRGB);
+}
 
