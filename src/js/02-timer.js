b9cb1834535188141bb02d818a8d6ad07fe80935
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-05-12 17:15:33 +0300

    task 2 restructuring

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,10 +6,106 @@ Notiflix.Notify.init({
   position: 'right-top',
   distance: '10px',
   opacity: 1,
-  clickToClose: true,
+  clickToClose: true ,
 });
 
 import "flatpickr/dist/flatpickr.min.css";
+
+Notiflix.Notify.info('Choose a date in the future');
+
+// const refs = {
+//   start: document.querySelector('button[data-start]'),
+//   input: document.getElementById('datetime-picker'),
+
+//   days: document.querySelector('span[data-days]'),
+//   hours: document.querySelector('span[data-hours]'),
+//   minutes: document.querySelector('span[data-minutes]'),
+//   seconds: document.querySelector('span[data-seconds]'),
+// }
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   now: Date.now(),
+
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0].getTime());
+//       if (selectedDates[0].getTime() < this.now) {
+//         refs.start.disabled = true
+//         Notiflix.Notify.failure('Please choose a date in the future');
+//       } else {
+//         refs.start.disabled = false
+//         Notiflix.Notify.success('Date selected press start!');
+//     }
+//   },
+// };
+
+// let intervalID = null
+// let isActive = false
+// let tick = null
+// refs.start.disabled = true
+// const fp = flatpickr("#datetime-picker", options)
+
+// refs.start.addEventListener('click', timerStart)
+
+// function timerStart() {
+
+//   let date = new Date(refs.input.value);
+  
+//   if (isActive) {
+//     return
+//   }
+
+//   isActive = true
+
+//   intervalID = setInterval(() => {
+//     let currentTime = Date.now();
+
+//     refs.start.disabled = true
+//     tick = date - currentTime
+//     console.log(tick);
+//     if (tick > 0) {
+//       const convert = convertMs(tick)
+//       console.log(`${pad(convert.days)} : ${pad(convert.hours)} : ${pad(convert.minutes)} : ${pad(convert.seconds)}`);
+
+//       refs.days.textContent = pad(convert.days)
+//       refs.hours.textContent = pad(convert.hours)
+//       refs.minutes.textContent = pad(convert.minutes)
+//       refs.seconds.textContent = pad(convert.seconds)
+//     } else {
+//       Notiflix.Notify.info('Time is up');
+//       clearInterval(intervalID);
+//       isActive = false
+//     }
+//   }, 1000);
+
+// }
+
+//   function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+// function pad(number) {
+// return String(number).padStart(2, '0')
+// }
+
 
 const refs = {
   start: document.querySelector('button[data-start]'),
@@ -34,52 +130,50 @@ const options = {
         refs.start.disabled = true
         Notiflix.Notify.failure('Please choose a date in the future');
       } else {
-        refs.start.disabled = false 
-    } 
+        refs.start.disabled = false
+        Notiflix.Notify.success('Date selected press start!');
+    }
   },
 };
 
-let intervalID = null
-let isActive = false
-let tick = null
-refs.start.disabled = true
-const fp = flatpickr("#datetime-picker", options)
+const fp = flatpickr("#datetime-picker", options);
+refs.start.disabled = true;
 
-refs.start.addEventListener('click', timerStart)
-
-function timerStart() {
-
-  let date = new Date(refs.input.value);
-  
-  if (isActive) {
-    return
+class Timer {
+  constructor({onTick}) {
+  this.intervalID = null
+  this.isActive = false
+    this.tick = null
+    this.onTick = onTick
   }
 
-  isActive = true
-
-  intervalID = setInterval(() => {
-    let currentTime = Date.now();
-
-    refs.start.disabled = true
-    tick = date - currentTime
-    console.log(tick);
-    if (tick > 0) {
-      const convert = convertMs(tick)
-      console.log(`${pad(convert.days)} : ${pad(convert.hours)} : ${pad(convert.minutes)} : ${pad(convert.seconds)}`);
-
-      refs.days.textContent = pad(convert.days)
-      refs.hours.textContent = pad(convert.hours)
-      refs.minutes.textContent = pad(convert.minutes)
-      refs.seconds.textContent = pad(convert.seconds)
-    } else {
-      clearInterval(intervalID);
-      isActive = false
+  timerStart() {
+    let date = new Date(refs.input.value);
+  
+    if (this.isActive) {
+      return
     }
-  }, 1000);
 
-}
+    this.isActive = true
 
-  function convertMs(ms) {
+    this.intervalID = setInterval(() => {
+      let currentTime = Date.now();
+
+      refs.start.disabled = true
+      this.tick = date - currentTime
+      console.log(this.tick);
+      if (this.tick > 0) {
+        const convert = this.convertMs(this.tick)
+        this.onTick(convert);
+      } else {
+        Notiflix.Notify.info('Time is up');
+        clearInterval(this.intervalID);
+        this.isActive = false
+      }
+    }, 1000);
+  }
+    
+  convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
@@ -87,19 +181,35 @@ function timerStart() {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = this.pad(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = this.pad(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = this.pad(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = this.pad(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
 
-function pad(number) {
+pad(number) {
 return String(number).padStart(2, '0')
 }
+
+}
+
+function updateClock({ days, hours, minutes, seconds }) {
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.minutes.textContent = minutes;
+  refs.seconds.textContent = seconds;
+}
+
+const timer = new Timer({
+  onTick: updateClock,
+});
+
+
+refs.start.addEventListener('click', () => timer.timerStart())
 
 
